ab231a3ff1f861abb0bb0bbff592a7d7b3121888
Adam Atanas <adamatanas@gmail.com>
2023-08-25 16:39:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="EulerGPU",
     version="0.1",
     packages=find_packages(),
-    install_requires=["numpy", "math", "torch", "itertools"],
+    install_requires=["numpy", "torch"],
     author="Adam Atanas",
     author_email="adamatanas@gmail.com",
     description="Pytorch-based implementation of Euler registration using GPU acceleration.",
